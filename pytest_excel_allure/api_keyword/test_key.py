e247df134a57e96df26c407fe946b5718271a78f
being <810239165@qq.com>
2023-02-20 16:44:22 +0800

    提交代码

---
+++ pytest_excel_allure/api_keyword/test_key.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/2/18 14:53
+# @Author  : being℡
+# @File    : test_key.py
+# @Software: python3.9.5 win10
+
+for i in range(10):
+    i = i+1
+    print(i)
